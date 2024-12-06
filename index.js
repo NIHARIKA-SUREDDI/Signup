925b2162b00ffe19429b9935f3e3554a1d2fbe4e
NIHARIKA-SUREDDI <niharika.sureddi@gmail.com>
2024-12-06 13:13:59 +0530

    login function

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ function signup(userName) {
 
 console.log(signup("John")); 
 console.log(signup("NewUser")); 
+
+// Login function
+function login(userName, password) {
+   
+    const users = ["John", "JaneSmith", "Bob"];
+    const correctPassword = "Emp@123";
+
+    if (!users.includes(userName)) {
+        return "User Not Found, Please Signup";
+    } else if (password !== correctPassword) {
+        return "Wrong Password....";
+    } else {
+        return "Login Successful...";
+    }
+}
+
+
+console.log(login("John", "Emp@123")); git
+console.log(login("JaneSmith", "WrongPass")); 
+console.log(login("NewUser", "Emp@123")); 
